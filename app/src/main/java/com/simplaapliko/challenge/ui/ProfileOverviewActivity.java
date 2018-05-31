e6ee8ff51f00f02f183292063a6fe428f62f9e61
Oleg Kan <oleg.kan@hotmail.com>
2018-05-30 20:13:22 -0400

    initial commit

---
+++ app/src/main/java/com/simplaapliko/challenge/ui/ProfileOverviewActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Oleg Kan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.simplaapliko.challenge.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.simplaapliko.challenge.R;
+
+public class ProfileOverviewActivity extends AppCompatActivity {
+
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, ProfileOverviewActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_overview);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+}
